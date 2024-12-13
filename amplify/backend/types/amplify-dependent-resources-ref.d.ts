83526d35578d84090cec79ecf0474f24d424b2b1
Cristian Sarmiento <cristian.sarmiento@carejourney.com>
2024-12-13 08:25:24 -0300

    fx

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,32 +1,29 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "testsenseaisiteui": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-<<<<<<< Updated upstream
-=======
-  },
-  "function": {
-    "S3Trigger30aab066": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "storage": {
-    "testsenseaiSiteMediaStorage": {
-      "BucketName": "string",
-      "Region": "string"
-    }
->>>>>>> Stashed changes
-  }
-}
+  auth: {
+    testsenseaisiteui: {
+      AppClientID: 'string';
+      AppClientIDWeb: 'string';
+      CreatedSNSRole: 'string';
+      IdentityPoolId: 'string';
+      IdentityPoolName: 'string';
+      UserPoolArn: 'string';
+      UserPoolId: 'string';
+      UserPoolName: 'string';
+    };
+  };
+  function: {
+    S3Trigger30aab066: {
+      Arn: 'string';
+      LambdaExecutionRole: 'string';
+      LambdaExecutionRoleArn: 'string';
+      Name: 'string';
+      Region: 'string';
+    };
+  };
+  storage: {
+    testsenseaiSiteMediaStorage: {
+      BucketName: 'string';
+      Region: 'string';
+    };
+  };
+};
