e730db96eaff5a136289427133688cd43c5354e3
Cristian Sarmiento <cristian.sarmiento@carejourney.com>
2024-12-20 06:07:18 -0300

    setting up sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Amplify } from 'aws-amplify';
 import App from './App';
 import './index.css';
-
-const awsmobile = {
-  aws_project_region: 'us-east-1',
-};
-
-Amplify.configure(awsmobile);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
